e3820dc5bf49f16b191e6b9e73fbec5b46bd1e4d
Robert P Pereira <robertpaiva.df@gmail.com>
2022-09-23 15:32:11 -0300

    Controlando manualmente uma classe enumerada

--- src/main/java/com/example/workshop/entities/Order.java
+++ src/main/java/com/example/workshop/entities/Order.java
@@ -12,8 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.example.workshop.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
 
 @Entity // Anotations para que o JPA crie/atualize o banco de dados
 @Table(name = "tb_order")
@@ -24,9 +24,19 @@ public class Order implements Serializable {
 		// dados
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // define auto incremento no campo id
 	private Long id;
-	
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT") //pra garantir que o instant seja mostrado lá no Json no formato ISO8001 
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT") // pra garantir
+																											// que o
+																											// instant
+																											// seja
+																											// mostrado
+																											// lá no
+																											// Json no
+																											// formato
+																											// ISO8601
 	private Instant moment;
+
+	private Integer orderStatus;
 
 	// criando associações
 	@ManyToOne // cria uma chave estrangeira
@@ -36,11 +46,13 @@ public class Order implements Serializable {
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
+
 	}
 
 	public Long getId() {
@@ -57,6 +69,16 @@ public class Order implements Serializable {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public User getClient() {
