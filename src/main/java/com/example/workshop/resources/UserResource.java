4d0b8f28be2430bf6d040bbad60612cf49256582
Robert P Pereira <robertpaiva.df@gmail.com>
2022-09-29 17:29:22 -0300

    Implementando Delete de usuário e retornando o http 204 No Content

--- src/main/java/com/example/workshop/resources/UserResource.java
+++ src/main/java/com/example/workshop/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,12 +41,19 @@ public class UserResource {
 	}
 	
 	
-	@PostMapping //usa-se para setar
+	@PostMapping //usa-se para inserir
 	public ResponseEntity<User> insert(@RequestBody User obj){ //Usa-se notation @RequestBody para receber o objeto Jason
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj); //tudo isso para gerar o retorno 201 lá no Jason que é 201=criando objeto
 	}
+	
+	@DeleteMapping(value="/{id}") //usa-se para deletar
+	public ResponseEntity<Void> delete(@PathVariable Long id){ 
+		service.delete(id);		
+		return ResponseEntity.noContent().build(); //resposta sem corpo e o código http será 204
+	}
+	
 	
 	
 }
