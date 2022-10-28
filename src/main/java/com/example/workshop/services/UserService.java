7f02fc71f41237bff8e1f2cad56977a4a9a036ea
Robert P Pereira <robertpaiva.df@gmail.com>
2022-10-28 10:27:12 -0300

    Implementando Exception handling - delete A318

--- src/main/java/com/example/workshop/services/UserService.java
+++ src/main/java/com/example/workshop/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.workshop.entities.User;
 import com.example.workshop.repositories.UserRepository;
+import com.example.workshop.services.exceptions.DatabaseException;
 import com.example.workshop.services.exceptions.ResourceNotFoundException;
 
 @Service // registrando UserService como um Serviço do spring na camada de serviço
@@ -31,7 +34,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try{
+			repository.deleteById(id);
+		}catch (EmptyResultDataAccessException e){			
+			throw new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
--- src/main/java/com/example/workshop/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/example/workshop/services/exceptions/ResourceNotFoundException.java
@@ -3,7 +3,7 @@ package com.example.workshop.services.exceptions;
 public class ResourceNotFoundException extends RuntimeException{
 	private static final long serialVersionUID = 1L;
 
-	public ResourceNotFoundException(Object id) {
+	public ResourceNotFoundException(Object id) { 
 		super("Resource not found. id " + id);
 	}
 
